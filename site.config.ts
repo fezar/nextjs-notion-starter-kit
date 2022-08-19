af634139fd775854c715c7dd1e464848981683f1
fezar <ifezar@gmail.com>
2022-08-19 10:28:54 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -49,16 +49,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-navigationStyle: 'custom',
-navigationLinks: [
-    {
-    title: 'About',
-     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-   },
-   {
-     title: 'Contact',
-     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-   }
- ]
+  // navigationStyle: 'default'
+     navigationStyle: 'custom',
+     navigationLinks: [
+       {
+         title: 'About',
+         pageId: '2c53271e47464ca59f021907db220508'
+       },
+       {
+         title: 'Contact',
+         pageId: 'b8b7971e749b41f487a1d00c40eb7a0d'
+       }
+     ]
 })
