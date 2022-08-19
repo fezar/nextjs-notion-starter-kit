97f6b828b7080a155932b73a76330c2cf9f90bfe
fezar <ifezar@gmail.com>
2022-08-19 15:06:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId:9kuai9,
+  rootNotionSpaceId:nothings,
 
   // basic site info (required)
   name: '碎弦',
